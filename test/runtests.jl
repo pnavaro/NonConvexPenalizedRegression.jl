1bf659dd813544ddf8fc316378a9cf505ed6dbd2
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2024-07-19 10:40:35 +0200

    Add Aqua.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
+using Aqua
 using NonConvexPenalizedRegression
-using Test
 using Random
 using Statistics
+using Test
 
 include("test_gaussian.jl")
 include("test_binomial.jl")
 include("test_std.jl")
 include("test_scad.jl")
+
+@testset "Aqua.jl" begin
+  Aqua.test_all(NonConvexPenalizedRegression)
+end
